5b2c56e707db6c044ad5ad24a9340dabebc6dc7a
Gourav h <gouravbhardwaj2015@gmail.com>
2019-11-28 11:04:41 +0530

    simple react form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, withRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LoginForm extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  }
+
+  handleSubmit =(e)=> {
+   
+e.preventDefault();
+    const url = 'http://192.168.0.54:1338/signinUser';
+    const data = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    try {
+     fetch(url, {
+        method: 'POST', 
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res=> res.json()).then( res=> 
+      {
+        if(res.success){
+          alert(res.message);
+          this.props.history.push('/dashboard')
+        }
+        else if (res.error.code === 301){
+          alert(res.error.message);
+        } else if (res.error.code === 404){
+          alert(res.error.message);
+        } else if (res.error.code === 500){
+          alert(res.error.message);
+        }
+      });
+          
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+
+  render() {
+
+    return (
+  
+<div class="form-style-5">
+<form onSubmit={this.handleSubmit}>
+<fieldset>
+<legend> Login Form</legend>
+<input type="text" name="username" placeholder="Your Email" value={this.state.username} onChange={(e) => this.setState({ username: e.target.value })}/>
+<input type="password" name="password" placeholder="Password" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })}/>
+<button className="btn" type="submit">Log In</button>
+
+</fieldset>
+
+</form>
+                <ul>
+                    <li>Don't have an Account?  <Link to="/register">SignUp Here</Link></li>
+                </ul>
+</div>
+
+    );
+  }
+
 }
 
-export default App;
+export default withRouter(LoginForm);
